794e6c63bc0119813510ddaac691f6ae197a13a6
logeshpaul <logeshpaul@gmail.com>
2018-02-09 12:34:37 +0530

    minor change

--- Move-Zeros/move-zeros.js
+++ Move-Zeros/move-zeros.js
@@ -1,6 +1,6 @@
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @return {[]} nums Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
   // edge cases
